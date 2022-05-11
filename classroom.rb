49473d361906a0333dee4101c8b56374390fd799
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-11 15:18:02 +0300

    add relationship between studnet and classroom

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,18 @@
 class Classroom
+  attr_reader :students
   attr_accessor :label
 
   def initialize(label)
     @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
+
+  def remove_student(student)
+    @students.delete(student)
   end
 end
