ae46621eac3bba3dd50aeb647417da88658d4636
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-12 14:38:43 +0300

    Generate uuid for person

--- person.rb
+++ person.rb
@@ -1,10 +1,12 @@
 require './nameable'
+require 'securerandom'
 
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = SecureRandom.uuid
     @name = name
     @age = age
     @parent_permission = parent_permission
