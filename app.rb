ec8f39bcfe45fe86accf523442baa5766f0b9878
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-12 16:28:03 +0300

    improve code

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ class App
   def initialize
     @store = {
       persons: [],
-      books: [],
+      books: []
     }
   end
 
@@ -10,117 +10,123 @@ class App
     if @store[:books].length.zero?
       puts 'There is no book!'
     else
-      puts(@store[:books].map { |b| "Title: \"#{b.title}\", Author: \"#{p.author}\""})
+      puts(@store[:books].map { |b| "Title: \"#{b.title}\", Author: \"#{b.author}\"" })
     end
 
     puts
-    home()
+    home
   end
 
   def list_all_people
-    puts(@store[:persons].map { |p| "[#{p.class}] - id: #{p.id} | #{p.name} | #{p.age} years old"})
+    if @store[:persons].length.zero?
+      puts 'There is no people!'
+    else
+      puts(@store[:persons].map { |p| "[#{p.class}] - id: #{p.id} | #{p.name} | #{p.age} years old" })
+    end
 
-    home()
+    puts
+    home
   end
 
   def create_person
-    puts "What type of person to make?"
+    puts 'What type of person to make?'
     options = {
-      "1": "Student",
-      "2": "Teacher"
+      '1': 'Student',
+      '2': 'Teacher'
     }
 
-    for key, value in options do
-      puts "#{key}) #{value}"
-    end
+    options.each { |key, value| puts "#{key}) #{value}" }
 
-    chosen_option = "-10000"
-    until options.has_key?(chosen_option.to_sym)
-      puts "Choose a valid option: \r"
+    chosen_option = '-10000'
+    until options.key?(chosen_option.to_sym)
+      print 'Choose a valid option: '
       chosen_option = gets.chomp
     end
 
-    puts 'Name?'
+    print 'Name: '
     name = gets.chomp
-    puts 'Age?'
+    print 'Age: '
     age = gets.chomp
 
-    if chosen_option == "1"
-      puts 'Has parent permission? (Y/n)'
+    if chosen_option == '1'
+      print 'Has parent permission? (Y/n) '
       permission_input = gets.chomp.downcase
-      permission = permission_input == "n" ? false : true
+      permission = permission_input != 'n'
 
       require './student'
       the_person = Student.new(nil, age, name, parent_permission: permission)
     else
-      puts 'Specialization?'
+      print 'Specialization: '
       specialization = gets.chomp
 
       require './teacher'
       the_person = Teacher.new(specialization, age, name)
     end
-    
+
     @store[:persons] << the_person
-    
-    home()
+    puts 'Saved.'
+
+    puts
+    home
   end
 
   def create_book
-    puts "Please add details of the book"
+    puts 'Please add details of the book'
 
-    puts 'Book Title?'
+    print 'Title: '
     title = gets.chomp
-    puts 'Author\'s name?'
+    print 'Author: '
     author = gets.chomp
 
     require './book'
-
     the_book = Book.new(title, author)
 
     @store[:books] << the_book
-    
-    home()
+    puts 'Saved.'
+
+    puts
+    home
   end
 
   def home
-    puts "Choose an option by entering its number:"
+    puts 'Here is the task list:'
     options = {
-      "1": "List all books",
-      "2": "List all people",
-      "3": "Create a person",
-      "4": "Create a book",
-      "5": "Create a rental",
-      "6": "List all rentals for a given person id",
-      "7": "Exit"
+      '1': 'List all books',
+      '2': 'List all people',
+      '3': 'Create a person',
+      '4': 'Create a book',
+      '5': 'Create a rental',
+      '6': 'List all rentals for a given person id',
+      '7': 'Exit'
     }
 
-    for key, value in options do
-      puts "#{key}) #{value}"
-    end
+    options.each { |key, value| puts "#{key}) #{value}" }
 
-    chosen_option = "-10000"
-    until options.has_key?(chosen_option.to_sym)
-      puts "Choose a valid options: \r"
+    chosen_option = '-10000'
+    until options.key?(chosen_option.to_sym)
+      print 'Enter the number of the task: '
       chosen_option = gets.chomp
     end
 
-    puts "Ok! You want to #{options[chosen_option.to_sym].downcase}"
+    puts "Ok! You want to #{options[chosen_option.to_sym].downcase}.\n\n"
+
     case chosen_option
-    when "1"
-      list_all_books()
-    when "2"
-      list_all_people()
-    when "3"
-      create_person()
-    when "4"
-      create_book()
-    when "7"
-      puts "See you soon. Bye! :)"
-      exit()
+    when '1'
+      list_all_books
+    when '2'
+      list_all_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '7'
+      puts 'See you soon. Bye! :)'
+      exit
     end
   end
+
   def run
     puts "Welcome. Tell me what should I do for you.\n\n"
-    home()
+    home
   end
 end
