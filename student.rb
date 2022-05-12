86d7d9253990f417086786e565b1626c56a59d00
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-12 14:10:02 +0300

    make it possible to create studnet without classroom

--- student.rb
+++ student.rb
@@ -1,4 +1,5 @@
 require './person'
+require './classroom'
 
 class Student < Person
   attr_reader :classroom
@@ -13,6 +14,8 @@ class Student < Person
   end
 
   def classroom=(new_classroom)
+    # only set classroom if we get a new_classroom of type Classroom
+    return nil unless new_classroom.is_a?(Classroom)
     # remove student from classroom if we get a new classroom
     @classroom.remove_student(self) unless @classroom.nil? || @classroom == new_classroom
 
