4dcfaa051bbccbc8423de1029c45804e969a05aa
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-18 17:43:05 +0300

    add test for book id

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -14,4 +14,20 @@ describe Book do
       expect(book.author).to eq "John Doe"
     end
   end
+
+  context "initialization with id" do
+    title = "Dev"
+    author = "John Doe"
+     
+    it "generates unique id by default" do
+      book = Book.new(title, author) 
+      id = book.id
+      expect(id.length).to be 36
+    end
+    it "generates unique id by default" do
+      expected_id = '48245d31-3ef7-48e3-bf27-08f85f033977'
+      book = Book.new(title, author, id: expected_id) 
+      expect(book.id).to be expected_id
+    end
+  end
 end
