6493c16dc26125739a9c81ae536d5dc177ca8cff
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-18 22:53:01 +0300

    organize test

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,16 +1,18 @@
 require './student'
 
-describe 'basic initialisation of Student class' do
-  name = 'dan'
-  age = 14
-  classroom = nil
-  student = Student.new(classroom, age, name)
+describe Student do
+  context 'basic initialisation of Student class' do
+    name = 'dan'
+    age = 14
+    classroom = nil
+    student = Student.new(classroom, age, name)
 
-  it 'should be equal to student name' do
-    expect(student.name).to eq 'dan'
-  end
+    it 'should be equal to student name' do
+      expect(student.name).to eq 'dan'
+    end
 
-  it 'should be equal to student age' do
-    expect(student.age).to be 14
+    it 'should be equal to student age' do
+      expect(student.age).to be 14
+    end
   end
 end
