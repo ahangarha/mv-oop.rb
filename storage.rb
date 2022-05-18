6fa51ff09c14713af57240cd091dbc7115dab6f5
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-18 13:35:36 +0300

    add load method

--- storage.rb
+++ storage.rb
@@ -12,6 +12,12 @@ class Storage
     File.write(filename, serialized_data)
   end
 
+  def load
+    filename = "#{@title}.json"
+    raw_data = File.read(filename)
+    JSON.parse(raw_data)
+  end
+
   def serialize(data)
     data_to_proccess = data.map(&:to_hash)
     JSON.pretty_generate(data_to_proccess)
