76f740ec73b66212e34c69e9efead95160437c85
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-18 22:55:59 +0300

    add test for student.to_hash method

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -15,4 +15,27 @@ describe Student do
       expect(student.age).to be 14
     end
   end
+
+  context 'test :to_hash method' do
+    it 'generate correct hash of the object' do
+      classroom = nil
+      age = 36
+      name = 'Reza'
+      id = 'blah'
+      parent_permission = false
+
+      student = Student.new(classroom, age, name, parent_permission: parent_permission, id: id)
+
+      expected_hash = {
+        class: 'Student',
+        id: id,
+        name: name,
+        age: age,
+        parent_permission: parent_permission,
+        classroom: classroom
+      }
+
+      expect(student.to_hash).to eq expected_hash
+    end
+  end
 end
